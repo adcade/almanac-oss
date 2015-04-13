b7c7f92b49c132fda06b33ce3258314b0f51cbba
Yangming Huang <yh@adcade.com>
2015-04-13 18:27:21 -0400

    experimenting cassandra and spark

---
+++ src/main/scala/almanac/spark/SparkAggregator.scala
@@ -0,0 +1,31 @@
+package almanac.spark
+
+import almanac.model.Metric._
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.util.Random
+
+class SparkAggregator {
+
+}
+
+object SparkAggregator {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("almanac").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val facts = Seq(withFacts("f1" -> "a", "f2" -> "1"),
+                    withFacts("f1" -> "b", "f2" -> "2"),
+                    withFacts("f3" -> "c", "f2" -> "3"))
+    val ranGen = new Random
+    val metrics = 1 to 1000 map (n => facts(ranGen nextInt facts.size) increment "some.counter")
+
+    val intermediaRDD = sc.parallelize(metrics) map (m => (m.key -> m.value)) reduceByKey (_ + _)
+
+    intermediaRDD.cache()
+
+    val result = intermediaRDD.collect()
+
+    println(result)
+  }
+}
