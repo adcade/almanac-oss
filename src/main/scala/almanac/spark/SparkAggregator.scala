ee1b6d050c61adbf911dbd97fd9d72e57f364677
Yangming Huang <yh@adcade.com>
2015-04-13 19:32:24 -0400

    map back to metrics

--- src/main/scala/almanac/spark/SparkAggregator.scala
+++ src/main/scala/almanac/spark/SparkAggregator.scala
@@ -1,5 +1,6 @@
 package almanac.spark
 
+import almanac.model.Metric
 import almanac.model.Metric._
 import almanac.model.TimeSpan._
 import org.apache.spark.{SparkConf, SparkContext}
@@ -21,12 +22,12 @@ object SparkAggregator {
     val ranGen = new Random
     val metrics = 1 to 1000 map (n => facts(ranGen nextInt facts.size) increment "some.counter")
 
-    val intermediaRDD = sc.parallelize(metrics) map (m => (m.key | MONTH) -> m.value) reduceByKey (_ + _)
+    val result = sc.parallelize(metrics)
+      .map(m => (m.key | MONTH) -> m.value)
+      .reduceByKey(_ + _)
+      .map(t => Metric(t._1, t._2))
+      .collect()
 
-    intermediaRDD.cache()
-
-    val result = intermediaRDD.collect()
-
-    println(result)
+    result.foreach(println)
   }
 }
