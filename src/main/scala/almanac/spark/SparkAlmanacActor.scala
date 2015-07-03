bb44cb83082783f2f1f2f8c7c04faa19b7989594
Yangming Huang <yh@adcade.com>
2015-07-02 23:14:55 -0400

    maybe actorStream is better?

--- src/main/scala/almanac/spark/SparkAlmanacActor.scala
+++ src/main/scala/almanac/spark/SparkAlmanacActor.scala
@@ -6,11 +6,12 @@ import almanac.model.Metric
 import almanac.persist.CassandraMetricRDDRepository
 import almanac.service.MetricsProtocol.{Query, QueryResult, Record}
 import almanac.spark.SparkMetricsAggregator._
-import almanac.util.RandomMetricsReceiver
-import org.apache.spark.storage.StorageLevel._
-import org.apache.spark.streaming.receiver.Receiver
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.receiver.ActorHelper
 import org.apache.spark.streaming.{Milliseconds, Seconds, StreamingContext}
 import org.apache.spark.{Logging, SparkConf, SparkContext}
+
+import scala.collection.mutable.ListBuffer
 
 class SparkAlmanacActor extends Actor with Logging {
   println("start constructor")
@@ -29,11 +30,13 @@ class SparkAlmanacActor extends Actor with Logging {
 
   implicit val repo = new CassandraMetricRDDRepository(sc, schedules)
 
-  val receiver = new MetricsReceiver
+  val storageLevel: StorageLevel = StorageLevel.MEMORY_AND_DISK_SER_2
 
-  //  val metricsStream = ssc receiverStream new MetricsReceiver
+  val metricsStream = ssc.actorStream[Metric](Props(classOf[MetricsReceiver], self.path), "MetricReceiver", storageLevel, supervisorStrategy)
+
+//  val metricsStream = ssc receiverStream new RandomMetricsReceiver
   // TODO: dynamic name?
-  val metricsStream = ssc receiverStream receiver
+//  val metricsStream = ssc receiverStream receiver
 
   metricsStream aggregateWithSchedule schedules
   metricsStream window(Seconds(10), Seconds(10)) count() print()
@@ -47,15 +50,25 @@ class SparkAlmanacActor extends Actor with Logging {
     println("finish to initialize")
   }
 
+  val receivers: ListBuffer[ActorRef] = new ListBuffer[ActorRef]()
+
   def receive: Receive = {
-    case Record(metrics) => println("record")
-//      receiver receive metrics
+    case Record(metrics) =>
+      receivers.foreach(_ ! Record(metrics))
 
     case Query(query) =>
       println("query")
       // TODO: call aggregator do facts group, ordering and limit/paging
       val resultRDD = repo read query aggregateByFacts query.groupNames
       sender ! QueryResult(resultRDD collect())
+
+    case SubscribeReceiver(receiverActor: ActorRef) =>
+      println("received subscribe from %s".format(receiverActor.toString))
+      receivers += receiverActor
+
+    case UnsubscribeReceiver(receiverActor: ActorRef) =>
+      println("received unsubscribe from %s".format(receiverActor.toString))
+      receivers.dropWhile(x => x eq receiverActor)
   }
 
   override def postStop() = StreamingContext getActive() foreach {
@@ -63,17 +76,21 @@ class SparkAlmanacActor extends Actor with Logging {
   }
 }
 
-class MetricsReceiver extends Receiver[Metric](MEMORY_ONLY) with Logging {
-  def receive(metrics: Seq[Metric]) = {
-    try {
+case class SubscribeReceiver(receiverActor: ActorRef)
+case class UnsubscribeReceiver(receiverActor: ActorRef)
+
+class MetricsReceiver(publisherPath: ActorPath) extends Actor with ActorHelper with Logging {
+  println(publisherPath)
+  println(self.path)
+  val publisher = context.actorSelection(publisherPath)
+
+  override def preStart(): Unit = publisher ! SubscribeReceiver(context.self)
+
+  def receive = {
+    case Record(metrics) =>
       logInfo(s"Sending: ${metrics.size} metrics")
-      store(metrics.iterator)
-      // TODO: restart if stopped ??? what about all the messages received before restart complete?
-    } catch {
-      case t: Throwable => restart("Error receiving data", t)
-    }
+      store(metrics)
   }
 
-  override def onStart(): Unit = {}
-  override def onStop(): Unit = {}
+  override def postStop(): Unit = publisher ! UnsubscribeReceiver(context.self)
 }
