4cfa5b8c436ab31651c14b44d8df4ff252576e22
Yangming Huang <yh@adcade.com>
2015-07-16 15:43:15 -0400

    adding more docs

--- README.md
+++ README.md
@@ -1,5 +1,106 @@
-# almanac
+# Almanac
+## Design
+
+Adcade or many other companies the metrics problems they are solving are different, but deep down it's similar. We need a generic solution that can cover problems like this: tracking metrics that is geo, temporal and some other non-numeric dimensions. each dimension have it's own granularity, time can be `SECOND`, `MINUTE`, `HOUR`, `DAY`, etc. geo locations can be grouped in to `GeoRect` that is equivalent to geohashes that the precision is decided by how many digit in the hashes are retained. And the non-numeric dimension, can be anything that is a key-value string pair. GeoTemporal project is certainly much simpler, it only concerns one dimension at a time: time dimension for question 3, geo dimension for question 1 and question 2.
+
+There is quite a few key points we'll tackled here
+
+### Goal 1 easy to record
+Goal 1 is to have a simple, hassel-free way to record metrics. This is why people love statsd. But they hate it because it's not feature rich. Dimension and geo are all missing. It's hard to have flexibility and simplicity in the same time. Almanac simply put everything into a single data model called `Metric`. And a collection of Builder api is available for easy metric creation.
+
+### Goal 2 query language
+With all these dimensions, data could grow exponentially. Every data-centric application needs to have a easy interface to retrieve data. Almanac is designed with a fluent API similar to SQL syntax and have a plan to make it into a real query language (AQL) in the future. So that you can record and retrieve the metrics in a intuitive way. Please scroll down to API for detail
+
+### Goal 3 streaming
+One of the most important goal is to handle thousand of metrics concurrently. For fast message delivery, there is not too much popular options to choose from right now. Kinesis and Kafka is on the top list, while zeromq and the old good socket is the easiest and still fast solution (which requires you to handle a lot of problem yourself) Luckily, Spark Streaming supports them all and more! Since we don't have credential to share for Kinesis, I picked kafka (which I've never used before, it's fun to learn).
+
+### Goal 4 aggregation
+Pre-aggregation of data is very necessary, so that they become less in amount, with different granularity. For aggregation, map-reduce is powerful. But spark is even better, which has a much easier interface, in-memo storage, and streaming. So data can be aggregated on the fly even before they been saved to database.
+
+### Goal 5 Storage
+How to easily retrieve the data and in short response time is the key of the problem. To archive that, we need to
+1. Process as less data as possible.
+2. Distributed data for scalability
+3. Locally data should be sequential for fast scan.
+
+Distributed Column-based database is the first come to mind, because you can have both distribution and local clustring. There are reasons cassandra are popular. You design the partition key and the clustering key in the way you want, and have a easy query language to interact with. Know how your data is stored so that you can optimize the access. And cassandra has a connector to easy connect with spark. When retieve the preaggregated data, you still need to aggregate facts in real time (because you have no way to know what's the combination of facts to group before query) And keep cassandra and spark partition in sync and aggregate locally before collect to the driver program is a good approach.
+
+### Goal 6 Deployment
+For a modern software. Easy deployment is important. Docker and Docker compose does simplify all that!!! In production, I would like to have mesos with docker containers. Spark and Cassandra are all deep integrated with mesos as well. Unfortunately, I did't archieve a "one line starter" at this point given time limitation (the almanac and geotemporal container doesn't play with the dependencies some how).
+
+## Demo
+
+### Prerequisite to run
+
+java > 7, sbt 0.13.8, docker and docker-compose
+
+### Installation and run
+
+in the root folder (which contains both almanac and geotemporal)
+``` bash
+docker-compose up # only for zookeeper, casssandra, kafka at this time
+
+sbt run # the driver program of spark to control aggregation, and at this moment, it also starts spark locally.
+```
+
+## Almanac
 
 almanac is a multi-dimension metrics framework.
 It supports StatsD similar interface, pre-aggregate by time and geo location, and provide a SQL like fluent api to query the metrics.
-This new project almanac-oss has re-write the original one in akka, spark and cassandra, and mean to become Open Source
+This new project almanac-oss has re-write the original one in akka, spark, cassandra and kafka
+
+### API
+
+currently, this verison of almanac only supports 2 api:
+
+#### record
+
+``` scala
+	almanac record Seq(metric1, metric2)
+```
+
+sample to make a metric
+```scala
+import almanac.model.Metric._
+metric decrement "some.bucket"
+metric withFacts("type" -> "uberx") increment "some.bucket"
+metric locate Coordinate(40.72, -73.25) increment("another.bucket", 13)
+metric at new DateTime(2015, 4, 15, 12, 00, 00).getTime gauge("bucket.of.gauge", 12)
+```
+
+you can also reuse the builder object
+
+```scala
+val builder = metric withFacts("type" -> "uberx", "region" -> "us-east")
+builder increment "some.bucket"
+builder increment "another.bucket"
+```
+
+#### query
+
+```scala
+	almanac retieve query
+```
+
+minimal query
+
+```scala
+select("std.impression").query
+```
+
+a complete query
+
+```scala
+select("std.impression", "std.exit")
+    .where( (fact("device") is "iphone")
+        and (fact("adId") in ("10001", "10002"))
+        and (fact("os") like """*ios*""") )
+    .groupBy("adId", "site")
+    .orderBy("adId", Order.ASC)
+    .locate(GeoRect("dr5ru"), 6)
+    .time(DAY, 1420020000000L, 1420106400000L)
+    .limit(1000)
+    .skip(2000)
+    .query
+```
+note that where, orderby, limit, skip clause of this version of almanac is not implemented.
--- src/main/scala/almanac/model/MetricQuery.scala
+++ src/main/scala/almanac/model/MetricQuery.scala
@@ -1,6 +1,7 @@
 package almanac.model
 
 import java.util.Date
+
 import almanac.model.GeoFilter.GlobalFilter
 import almanac.model.GeoHash._
 import almanac.model.TimeFilter.EverFilter
@@ -11,10 +12,11 @@ object TimeFilter {
 }
 
 /**
+ * Filter by a time period of certain `TimeSpan` (precision of Time) from `fromTime` to `toTime`
  *
- * @param span
- * @param fromTime
- * @param toTime
+ * @param span the Time Span to be filtered
+ * @param fromTime the inclusive start point of the
+ * @param toTime the exclusive end point of the time period
  */
 case class TimeFilter(span: TimeSpan, fromTime: Long, toTime: Long) {
   override def toString: String =
@@ -31,9 +33,11 @@ object GeoFilter {
 }
 
 /**
+ * Filter by a geo area (`GeoShape`) with a max precision that is subject to the availability of
+ * aggregated geo precisions
  *
- * @param shape
- * @param maxPrecision
+ * @param shape the geo area to be filtered
+ * @param maxPrecision the max precision to return
  */
 // TODO geofilter for null geohash
 case class GeoFilter(shape: GeoShape, maxPrecision: Int) {
@@ -48,9 +52,12 @@ object Order extends Enumeration {
 }
 
 /**
+ * Order by facts only
+ * metrics are first ordered by bucket and then time and then geo by default
+ * facts ordering is applied after the default ordering
  *
- * @param fact
- * @param dir
+ * @param fact the fact to be ordered
+ * @param dir the order direction for the fact, either ASC or DESC
  */
 case class Order private[model] (fact: String, dir: Order.Direction) {
   override def toString: String = fact + " " + dir
@@ -58,12 +65,12 @@ case class Order private[model] (fact: String, dir: Order.Direction) {
 
 /**
  *
- * @param buckets
- * @param criteria
- * @param groupNames
- * @param orders
- * @param geoFilter
- * @param timeFilter
+ * @param buckets the buckets to be selected
+ * @param criteria the criteria of the facts
+ * @param groupNames the fact names to be grouped by
+ * @param orders the orders of the facts
+ * @param geoFilter the geo filter
+ * @param timeFilter the time filter
  */
 case class MetricsQuery (
   buckets: Set[String],
@@ -74,13 +81,12 @@ case class MetricsQuery (
   timeFilter: TimeFilter) {
 
   /**
-   * unwounded queries with each buckets for caching purpose
+   * Unwounded queries with each buckets for caching purpose
    *
    * TODO: make geoFilter also a part of the unwind process
    *
-   * @return
+   * @return a list of sub-queries that the union of whose results is equivalent to the query itself
    */
-  // TODO: need to work on geoFilter
   def unwoundQueries = for {
     b <- buckets
   } yield MetricsQuery(Set(b), criteria, groupNames, orders, geoFilter, timeFilter)
@@ -90,13 +96,13 @@ case class MetricsQuery (
     sb append "SELECT " append buckets append '\n'
     if (criteria != NonCriteria)
       sb append "WHERE " append criteria append '\n'
-    if (groupNames.size > 0)
+    if (groupNames.nonEmpty)
       sb append "GROUP BY " append groupNames append '\n'
-    if (orders.size > 0)
+    if (orders.nonEmpty)
       sb append "ORDER BY " append orders append '\n'
     sb append "TIME " append timeFilter append '\n'
     sb append "GEO " append geoFilter append '\n'
-    sb.toString
+    sb.toString()
   }
 }
 
@@ -114,93 +120,111 @@ object MetricsQuery {
     skip: Int) {
 
     /**
+     * Filter by facts who meets the criteria
      *
-     * @param criteria
-     * @return
+     * @param criteria the criteria of facts
+     * @return a new MetricQuery.Builder that meets the above condition
      */
     def where(criteria: Criteria) = Builder(buckets, criteria, groupNames, orders, geoFilter, timeFilter, limit, skip)
 
     /**
+     * Group by facts, reduce metrics where designated facts are the same
+     * in other word, any fact that is not included in the parameter list will be eliminated
      *
-     * @param facts
-     * @return
+     * @param facts the facts to be grouped by
+     * @return a new MetricQuery.Builder that meets the above condition
      */
     def groupBy(facts: String*) = Builder(buckets, criteria, groupNames ++ facts, orders, geoFilter, timeFilter, limit, skip)
 
     /**
+     * Order by facts only
+     * metrics are first ordered by bucket and then time and then geo by default
+     * facts ordering is applied after the default ordering
      *
-     * @param orders
-     * @return
+     * @param orders the order object which contains both the fact to be ordered and the direction
+     * @return a new MetricQuery.Builder that meets the above condition
      */
     def orderBy(orders: Order*) = Builder(buckets, criteria, groupNames, this.orders ++ orders, geoFilter, timeFilter, limit, skip)
 
     /**
+     * Order by facts only
+     * metrics are first ordered by bucket and then time and then geo by default
+     * facts ordering is applied after the default ordering
      *
-     * @param fact
-     * @param dir
-     * @return
+     * @param fact the fact to be ordered
+     * @param dir the order direction for the fact, either ASC or DESC
+     * @return a new MetricQuery.Builder that meets the above condition
      */
     def orderBy(fact: String, dir: Order.Direction): Builder = orderBy(Order(fact, dir))
 
     /**
+     * Filter the metrics with a `TimeFilter`
      *
-     * @param filter
-     * @return
+     * @param filter the time filter
+     * @return a new MetricQuery.Builder that meets the above condition
      */
     def time(filter: TimeFilter) = Builder(buckets, criteria, groupNames, orders, geoFilter, filter, limit, skip)
 
     /**
+     * Filter by a time period of certain `TimeSpan` (precision of Time) from `fromTime` to `toTime`
      *
-     * @param span
-     * @param fromTime
-     * @param toTime
-     * @return
+     * @param span the Time Span to be filtered
+     * @param fromTime the inclusive start point of the
+     * @param toTime the exclusive end point of the time period
+     * @return a new MetricQuery.Builder that meets the above condition
      */
     def time(span: TimeSpan, fromTime: Long, toTime: Long): Builder = time(TimeFilter(span, fromTime, toTime))
 
     /**
+     * Filter the metrics with a `GeoFilter`
      *
-     * @param filter
-     * @return
+     * @param filter the geo filter
+     * @return a new MetricQuery.Builder that meets the above condition
      */
-    def locate(filter: GeoFilter): Builder = Builder(buckets, criteria, groupNames, orders, filter, timeFilter, limit, skip)
+    def locate(filter: GeoFilter) = Builder(buckets, criteria, groupNames, orders, filter, timeFilter, limit, skip)
 
     /**
+     * Filter by the geo area represented by the geohash
      *
-     * @param geohash
-     * @return
+     * @param geohash the geohash of the geo area to be filtered
+     * @return a new MetricQuery.Builder that meets the above condition
      */
     def locate(geohash: String): Builder = locate(GeoFilter(geohash))
 
     /**
+     * Filter by a geo area (`GeoShape`) with a max precision that is subject to the availability of
+     * aggregated geo precisions
      *
-     * @param shape
-     * @param maxPrecision
-     * @return
+     * @param shape the geo area to be filtered
+     * @param maxPrecision the max precision to return
+     * @return a new MetricQuery.Builder that meets the above condition
      */
     def locate(shape: GeoShape, maxPrecision: Int = MAX_PRECISION): Builder = locate(GeoFilter(shape, maxPrecision))
 
     /**
+     * Limit for paginating
      *
-     * @param limit
-     * @return
+     * @param limit maximum metrics returned
+     * @return a new MetricQuery.Builder that meets the above condition
      */
-    def limit(limit: Int): Builder = Builder(buckets, criteria, groupNames, orders, geoFilter, timeFilter, limit, skip)
+    def limit(limit: Int) = Builder(buckets, criteria, groupNames, orders, geoFilter, timeFilter, limit, skip)
 
     /**
+     * Skip for paginating
      *
-     * @param skip
-     * @return
+     * @param skip how many metrics to skip
+     * @return a new MetricQuery.Builder that meets the above condition
      */
-    def skip(skip: Int) = Builder(buckets, criteria, groupNames, orders, geoFilter, timeFilter, limit, skip)
+    def skip(skip: Int): Builder = Builder(buckets, criteria, groupNames, orders, geoFilter, timeFilter, limit, skip)
 
     lazy val query = MetricsQuery(buckets, criteria, groupNames, orders, geoFilter, timeFilter)
   }
 
   /**
+   * Select all the buckets to be returned
    *
-   * @param buckets
-   * @return
+   * @param buckets a list of buckets to be selected
+   * @return a new MetricQuery.Builder that meets the above condition
    */
   def select(buckets: String*) = Builder(Set(buckets: _*), NonCriteria, Seq(), Seq(), GlobalFilter, EverFilter, NO_LIMIT, 0)
 }
